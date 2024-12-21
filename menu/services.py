1b73a501a4b2db34526ec0a717f1f70d9f48759d
Ayush Singhal <ayushsinghal998@gmail.com>
2024-12-21 23:45:13 +0530

    update qr menu creation code

--- menu/services.py
+++ menu/services.py
@@ -1,7 +1,6 @@
 import logging
 
 from django.db import transaction
-from rest_framework.exceptions import ValidationError
 
 from branch.services import BranchService
 from commons.service.BaseService import BaseService
@@ -17,21 +16,11 @@ class QRMenuService(BaseService):
         self.branch_service = branch_service or BranchService()
 
     def create(self, menu_data):
-        try:
-            branch_id = menu_data.get("branch_id")
-            self.branch_service.get_by_id(branch_id)
+        branch_id = menu_data.get("branch_id")
+        self.branch_service.get_by_id(branch_id)
 
-            qr_menu_serializer = QRMenuSerializer(data=menu_data)
-            if qr_menu_serializer.is_valid(raise_exception=True):
-                with transaction.atomic():
-                    qr_menu_instance = qr_menu_serializer.save()
-
-                return qr_menu_instance
-
-        except ValidationError as e:
-            logger.warning(f"Validation error while creating QR menu: {str(e)}")
-            raise
-
-        except Exception:
-            logger.error("Error while creating QR menu", exc_info=True)
-            raise
+        qr_menu_serializer = QRMenuSerializer(data=menu_data)
+        if qr_menu_serializer.is_valid(raise_exception=True):
+            with transaction.atomic():
+                qr_menu_instance = qr_menu_serializer.save()
+            return qr_menu_instance
--- menu/views.py
+++ menu/views.py
@@ -1,22 +1,19 @@
 from rest_framework.decorators import action
-from rest_framework.exceptions import ValidationError
 from rest_framework.viewsets import ViewSet
 
 from commons.api.responses import ResponseFactory
+from commons.middleware.api_handler import handle_api_exceptions
 from menu.serializer import QRMenuSerializer
 from menu.services import QRMenuService
 
 
 class QRMenuView(ViewSet):
+    def __init__(self, qr_menu_service=None, **kwargs):
+        super().__init__(**kwargs)
+        self.qr_menu_service = qr_menu_service or QRMenuService()
+
     @action(detail=False, methods=["post"], url_path="create")
+    @handle_api_exceptions
     def create_menu(self, request):
-        qr_menu_service = QRMenuService()
-
-        try:
-            qr_menu_instance = qr_menu_service.create(request.data)
-
-            return ResponseFactory.created(QRMenuSerializer(qr_menu_instance).data)
-        except ValidationError as e:
-            return ResponseFactory.bad_request(e)
-        except Exception:
-            return ResponseFactory.server_error()
+        qr_menu_instance = self.qr_menu_service.create(request.data)
+        return ResponseFactory.created(QRMenuSerializer(qr_menu_instance).data)

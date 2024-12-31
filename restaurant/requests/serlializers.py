c588e327fa61537df07e951fa3a6762fd7a00707
Ayush Singhal <ayushsinghal998@gmail.com>
2025-01-01 00:46:39 +0530

    fix request for create branch

--- restaurant/requests/serlializers.py
+++ restaurant/requests/serlializers.py
@@ -3,4 +3,6 @@ from rest_framework import serializers
 
 class RestaurantSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=100)
-    description = serializers.CharField(max_length=500, required=False)
+    description = serializers.CharField(
+        max_length=500, required=False, allow_blank=True
+    )

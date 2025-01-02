5fa30038fb6ee4da687cd526e28c38aad4fd0286
Ayush Singhal <ayushsinghal998@gmail.com>
2025-01-02 22:09:04 +0530

    update serializer

--- menu_file/serializer.py
+++ menu_file/serializer.py
@@ -1,10 +1,12 @@
 from rest_framework import serializers
 
 from commons.serializer.BaseModelSerializer import BaseModelSerializer
+from file_upload.enum.Buckets import BucketType
+from file_upload.enum.FIleType import FileType
+from file_upload.services import FileUploadService
 from menu.models import QRMenu
 
 from .models import MenuFile
-from .utils import get_url
 
 
 class MenuFileGetSerializer(BaseModelSerializer):
@@ -20,7 +22,9 @@ class MenuFileGetSerializer(BaseModelSerializer):
     @staticmethod
     def get_file_url(obj):
         if obj.file_key:
-            return get_url(obj.menu.branch_id, obj.file_key)
+            return FileUploadService(BucketType.MENU, FileType.QR_MENU).get_url(
+                obj.file_key, path_params={"branch_id": obj.menu.branch_id}
+            )
         return None
 
 

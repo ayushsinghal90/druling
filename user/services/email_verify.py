1b0a872f7a81232e18ba5325b19869803523d636
Ayush Singhal <ayushsinghal998@gmail.com>
2025-01-04 21:48:10 +0530

    fix code

--- user/services/email_verify.py
+++ user/services/email_verify.py
@@ -10,16 +10,14 @@ class EmailVerifyService:
     def __init__(self, redis_client=None):
         self.redis_client = redis_client or RedisClient()
 
-    def create_code(self, data):
+    def send_code(self, data):
         email = data.get("email")
 
         # Generate a 6-digit random code
         code = random.randint(100000, 999999)
 
-        # Store the code in Redis with a TTL (e.g., 10 minutes)
-        self.redis_client.set(
-            f"email_verification:{email}", code, 600
-        )  # Key expires in 600 seconds
+        # Store the code in Redis with a TTL 10 min.
+        self.redis_client.set(f"email_verification:{email}", code, 600)
 
         # Send the code via email
         send_mail(
@@ -33,7 +31,6 @@ class EmailVerifyService:
         email = data.get("email")
         code = data.get("code")
 
-        # Retrieve the code from Redis
         stored_code = self.redis_client.get(f"email_verification:{email}")
         if stored_code is None or stored_code != code:
             return False

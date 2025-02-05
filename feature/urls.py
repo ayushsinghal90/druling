b6d0c664420fa5c7a37f6965f9d4b4e1da7a6b9b
Ayush Singhal <ayushsinghal998@gmail.com>
2025-02-05 21:11:01 +0530

    fix feature url

--- feature/urls.py
+++ feature/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path
-from .views import PlanView
+# from django.urls import path
+# from .views import PlanView
 
 urlpatterns = [
-    path("all/", PlanView.as_view({"get": "get_all_plans"})),
-    path("<plan_id>/", PlanView.as_view({"get": "get_plan_by_id"})),
+    # path("all/", PlanView.as_view({"get": "get_all_plans"})),
 ]

b7760268a9c3139649da5362f9a10a3fcef3f738
Ayush Singhal <ayushsinghal998@gmail.com>
2025-02-16 23:29:05 +0530

    Adding seat and order

---
+++ seat/models/seat.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from branch.models import Branch
+from commons.models.BaseModel import BaseModel
+from item.models import Item
+
+
+class Seat(BaseModel):
+    branch = models.ForeignKey(
+        Branch, related_name="customer_favourites", on_delete=models.CASCADE
+    )
+    item = models.ForeignKey(Item, related_name="favourites", on_delete=models.CASCADE)
+    note = models.CharField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.item.name} - {self.branch.name}"
+
+    class Meta:
+        db_table = "seat"

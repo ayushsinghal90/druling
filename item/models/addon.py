eda83023f45fe34def8cb77800d33ee1619b95ff
Ayush Singhal <ayushsinghal998@gmail.com>
2025-02-17 23:54:09 +0530

    remove unwanted fiels

--- item/models/addon.py
+++ item/models/addon.py
@@ -1,11 +1,9 @@
 from django.db import models
 
-from branch.models import Branch
 from commons.models.BaseModel import BaseModel
 
 
 class Addon(BaseModel):
-    branch = models.ForeignKey(Branch, related_name="addons", on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)

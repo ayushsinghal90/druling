71966ee65bfbe0364f4ed106898d67cc573b99de
Ayush Singhal <ayushsinghal998@gmail.com>
2025-02-18 09:56:58 +0530

    adding serializers

--- item/serializer/addon.py
+++ item/serializer/addon.py
@@ -1,31 +1,20 @@
 from rest_framework import serializers
 
-from branch.models import Branch
 from commons.serializer.BaseModelSerializer import BaseModelSerializer
 
 from ..models import Item
 
 
-class ItemSerializer(BaseModelSerializer):
-    branch_id = serializers.PrimaryKeyRelatedField(
-        queryset=Branch.objects.all(), required=True, source="branch"
-    )
+class AddonSerializer(BaseModelSerializer):
     ingredients = serializers.JSONField()
 
     class Meta:
         model = Item
         fields = [
             "id",
-            "branch_id",
             "name",
             "description",
             "price",
             "remaining_quantities",
             "ingredients",
         ]
-
-    def create(self, validated_data):
-        return Item.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        return super().update(instance, validated_data)

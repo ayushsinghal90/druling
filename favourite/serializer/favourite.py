b0172d46d3ac61e4b6d8eb208ef1f403c9b081f8
Ayush Singhal <ayushsinghal998@gmail.com>
2025-02-25 08:46:49 +0530

    adding favourite serializer and service

---
+++ favourite/serializer/favourite.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from branch.models import Branch
+from commons.serializer.BaseModelSerializer import BaseModelSerializer
+from item.models import Item
+
+from ..models import Favourite
+
+
+class FavouriteSerializer(BaseModelSerializer):
+    branch_id = serializers.PrimaryKeyRelatedField(
+        queryset=Branch.objects.all(), required=True, source="branch"
+    )
+    item_id = serializers.PrimaryKeyRelatedField(
+        queryset=Item.objects.all(), required=True, source="item"
+    )
+
+    class Meta:
+        model = Favourite
+        fields = [
+            "id",
+            "branch_id",
+            "item_id",
+        ]
---
+++ favourite/services/__init__.py
@@ -0,0 +1,4 @@
+from .customer_favourite import CustomerFavouriteService
+from .favourite import FavouriteService
+
+__all__ = [FavouriteService, CustomerFavouriteService]
